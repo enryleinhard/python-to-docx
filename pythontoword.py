1d27910a5a18cd81c9ef2b802c60dce3740e9afb
enryleinhard <enryl.einhard@gmail.com>
2019-07-28 21:24:21 +0700

    picture added

--- pythontoword.py
+++ pythontoword.py
@@ -1,6 +1,9 @@
 import docx
+from docx.shared import Inches
+
 
 doc = docx.Document()
 doc.add_paragraph('Hello world!')
+doc.add_picture('./test.jpg' , width=Inches(1.5748), height=Inches(2.3622))
 doc.add_paragraph('Hello world! x2')
 doc.save('demo3.docx')
