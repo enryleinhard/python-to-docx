0ac3dc5c92697a3d0e34c28eb9df719231012edb
enryleinhard <enryl.einhard@gmail.com>
2019-07-28 21:37:30 +0700

    picture added

--- pythontowordflask.py
+++ pythontowordflask.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_cors import CORS, cross_origin
 import docx
+from docx.shared import Inches
 
 application = app = Flask(__name__)
 api = Api(app)
@@ -15,12 +16,22 @@ api.add_resource(HelloWorld, '/')
 class text(Resource):
     def get(self, text, text2):
 
-        doc = docx.Document()
+        doc = docx.Document('pythontowordflask.docx')
         doc.add_paragraph(text)
         doc.add_paragraph(text2)
         doc.save('pythontowordflask.docx') 
 
 api.add_resource(text, '/text/<text>/<text2>')
+
+class getImage(Resource):
+    def get(self, gambar):
+        doc = docx.Document()
+        doc.add_picture('./'+gambar , width=Inches(1.5748), height=Inches(2.3622))
+        doc.save('pythontowordflask.docx')
+
+api.add_resource(getImage, '/image/<gambar>')
+
+# harus masukin image dl baru masukin text.
 
 if __name__ == '__main__':
     app.run(debug=True)
